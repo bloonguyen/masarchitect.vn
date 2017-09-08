f4fc321a1b711d017bb4b869b9946d4d704aa9a3
minhthai1995 <minhthai1995@gmail.com>
2017-09-08 10:51:10 +0700

    Layout lai post va press

--- client/components/press.jsx
+++ client/components/press.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Link, browserHistory} from 'react-router';
+import AvailablePost from 'client/components/getData.jsx'
 import globalStyles from 'client/styles/globalStyles.css';
 import blogStyles from '../layout/styles/blog_style.css';
 export default class Press extends React.Component {
@@ -28,50 +29,17 @@ export default class Press extends React.Component {
 	}
 	render() {
 		var nodeList = this.state.blogData.map((item,index) => {
-			return (<PressPost
-				 		data={item}
-						indexKey={index}
-						/>)
+			if (item.kieu == "press"){
+				return (<AvailablePost
+					 		data={item}
+							indexKey={index}
+							/>)
+			}
 		})
 		return (
 			<div>
 					{nodeList}
                     </div>
-		)
-	}
-}
-
-export class PressPost extends React.Component{
-    render() {
-		var date = (this.props.data.publishedDate)? new Date(this.props.data.publishedDate): new Date();
-		var parsedDate = (date.getMonth() + 1) + '/' + date.getDate() + '/' +  date.getFullYear();
-		if (this.props.data.kieu != "press") return null;
-		return (
-			<div className={globalStyles.main_flex_container}>
-				<div className={globalStyles.col_4}>
-					<div className={blogStyles.content_left_wrapper}>
-						<div className={blogStyles.content_left}>
-						<Link to={"/post/"+this.props.data.slug}>
-						<div style={{marginTop:'10px',fontSize:'18px', fontWeight:'bold', fontFamily:'HelveticaNeue-Light'}}>
-							{this.props.data.tieuDe}
-							</div>
-							<img style ={{width:'60%'}}src={this.props.data.hinhDaiDien.url}/>
-							<p>{parsedDate}</p>
-						</Link>
-						</div>
-					</div>
-				</div>
-				<div className={globalStyles.col_8}>
-					<div className={blogStyles.content_right}>
-
-
-						<p
-							style={{marginTop:'20px',fontSize:'18px', fontWeight:'no', fontFamily:'HelveticaNeue-Light'}}
-							dangerouslySetInnerHTML={{__html: this.props.data.noiDung.tomTat}}>
-						</p>
-					</div>
-				</div>
-			</div>
 		)
 	}
 }
