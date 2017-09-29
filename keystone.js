d3497a79d45a8836b7d8c6a923e8f1e1aa7854a8
Binh Nguyen <nguyenthaibinh.bl@gmail.com>
2017-09-29 15:12:02 +0700

    change wysiwyg buttons

--- keystone.js
+++ keystone.js
@@ -9,7 +9,7 @@ keystone.init({
 	'views': 'templates/views',
 	'view engine': 'ejs',
 	'custom engine': require('ejs').__express,
-	'wysiwyg additional buttons':'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+	'wysiwyg additional buttons':'undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
 	'wysiwyg override toolbar':true,
 	'auto update': true,
 	'session': true,
