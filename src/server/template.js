aaa6bd36ae32bf9758b530ca85d0d63178d49693
Binh Nguyen <nguyenthaibinh.bl@gmail.com>
2017-10-05 11:28:51 +0700

    checkout to fix staffCV missing

--- src/server/template.js
+++ src/server/template.js
@@ -2,9 +2,12 @@ export default (html) => `
     <!DOCTYPE html>
     <html>
     <head>
-        <title>test app</title>
+        <title>MAS Architecture</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'/>
+        <link href='/css/bootstrap.min.css' rel='stylesheet'>
+        <link href='/css/stylesheet.css' rel='stylesheet'>
+        <link rel="icon" type="image/png" href="/images/favicon.png">
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
     </head>
     <body>
         <div id="app">${html}</div>
