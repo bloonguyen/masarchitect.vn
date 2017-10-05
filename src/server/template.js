d481b2bd0a23e8320f9d94992844b3734fcd2f24
Binh Nguyen <nguyenthaibinh.bl@gmail.com>
2017-10-05 17:54:27 +0700

    fix meta tag

--- src/server/template.js
+++ src/server/template.js
@@ -6,6 +6,7 @@ export default (html) => `
         <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'/>
         <meta name='description' content='MAS Architecture design  civil & industrial architectural projects such as houses, villas, restaurant, hotel, bar, coffee, office ... With the slogan "Creativity - Experiences - Enthusiasm", MAS Architecture will bring you an active and comfortable living space.'/>
         <meta name='author' content='M.SPACE Co.,ltd'/>
+        <meta property="og:image" content="/images/cover.jpg">
         <link href='/css/bootstrap.min.css' rel='stylesheet'>
         <link href='/css/stylesheet.css' rel='stylesheet'>
         <link href='/bundle/styles.css' rel='stylesheet'>
